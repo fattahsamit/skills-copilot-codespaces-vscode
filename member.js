5c0d291f4f13242f99b0aafab1d98813f0283b56
fattahsamit <fattahsamit@gmail.com>
2025-04-22 17:55:39 +0600

    Add skillsMember function to define team member details

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  return {
+    name: "skillsMember",
+    description: "A member of the skills team",
+    skills: ["JavaScript", "Python", "Java"],
+    experience: 5,
+    projects: [
+      {
+        name: "Project A",
+        role: "Developer",
+      },
+      {
+        name: "Project B",
+        role: "Lead Developer",
+      },
+    ],
+  };
+}
